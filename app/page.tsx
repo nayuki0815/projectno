d7b79b728497b11df11e31799360b9dce5e9cfe9
中野 佑樹 <nakano.yuuki.0815@gmail.com>
2026-03-14 00:19:17 +0900

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>中野佑樹のサイト</h1>
-      <p>Next.jsで作った最初のサイトです。</p>
-    </main>
-  );
+	return (
+		<main className="relative min-h-screen overflow-hidden bg-black text-white">
+			<div className="absolute right-12 top-1/2 -translate-y-1/2 text-right md:right-20">
+				<h1 className="text-5xl font-light tracking-tight md:text-7xl">
+					project.no <span className="ml-6">-2nd</span>
+				</h1>
+			</div>
+		</main>
+	);
 }
